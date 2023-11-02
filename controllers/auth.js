2ff36ac6d2dfb78b43157a19008250afe1d5b051
Vinh Quang Tran <vinhquangtran@Vinh-MacBook-Pro.local>
2023-11-02 11:54:26 +0100

    msg

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 
 const mysql = require('mysql');
 // const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 // const app = express();
 
 // app.use(session({
